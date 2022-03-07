6768e5fcedec732263ff913b4ca6c6aeed4ec889
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-03-07 09:18:53 +0530

    initial commit

---
+++ src/com/mod/Gambler.java
@@ -0,0 +1,12 @@
+package com.mod;
+
+public class Gambler {
+    int stake = 100;
+    int bet = 1;
+
+    public static void main(String[] args) {
+
+        System.out.println("stake");
+        System.out.println("bet");
+    }
+}
